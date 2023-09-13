5f86c14b5cb5ad72db50bda35bcf93f75d326595
Stoyan <stoyan.gyulemerov@gmail.com>
2023-09-13 18:21:55 +0300

    Add Exercise 11

---
+++ 03. JS Syntax Fundamentals/11-sum-first-and-last-array-elements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastElement(...input) {
+    let first = input[0];
+    let last = input[input.length - 1];
+    console.log(first + last);
+}
+
+sumFirstAndLastElement(1,2,3,4,5,6);
