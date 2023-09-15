45de7294f08446f5dd70d5059e06b8ddaa968846
Stoyan <stoyan.gyulemerov@gmail.com>
2023-09-15 15:43:44 +0300

    Add Exercise 5

---
+++ 04. Exercise JS Syntax Fundamentals/05-multiplication-table.js
@@ -0,0 +1,7 @@
+function createMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+createMultiplicationTable(5);
